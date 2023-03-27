e7a6b6e1151ddda083b960ec2d87aa0c54fd31d0
Dpetrov88 <117352418+Dpetrov88@users.noreply.github.com>
2023-03-27 14:17:35 +0300

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,69 @@
 import {createContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+import {authServiceFactory} from "../service/authService";
+
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const [user, setUser] = useLocalStorage('user', {});
+    const navigate = useNavigate();
+    const authService =  authServiceFactory(user.accessToken);
+
+    const onLoginSubmit = async(data) => {
+		try {
+			const result = await authService.login(data);
+			
+			setUser(result);
+			
+			navigate('/');
+
+		} catch (error) {
+			console.log("Problem!!");
+		}
+	};
+
+	const onRegisterSubmit = async(data) => { 
+		const {confirmPass , ...registerData } = data;
+
+		if (confirmPass !== registerData.password) {
+			return;
+		}
+		try {
+			const result = await authService.register(registerData);
+
+			setUser(result);
+
+			navigate('/');
+
+		} catch (error) {
+			console.log("Problem!");
+		}
+	};
+
+    const onLogout = async() => {
+        await authService.logout();
+
+       setUser({});
+   }
+
+    const contex = {
+		onLoginSubmit,
+		onRegisterSubmit,
+		onLogout,
+		userId: user._id,
+		token: user.accessToken,
+		email: user.email,
+		isAuthenticated: !!user.accessToken,
+
+	}
+    return (
+        <AuthContext.Provider value={contex}>
+             {children }
+        </AuthContext.Provider>
+    )
+};
