a6d73d94fed91ba9d2924a95cf3a120d41baa04d
Dpetrov88 <117352418+Dpetrov88@users.noreply.github.com>
2023-03-30 18:24:32 +0300

    Update Details.js

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,13 +1,19 @@
 import { useParams, useNavigate, Link} from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import {mattressServiceFactory}from '../../service/mattressService';
 import { useService } from "../../hooks/useService";
 import { AuthContext } from "../../contexts/AuthContext";
+import { useMattressContex } from "../../contexts/MattresContext";
 
 export const Details = () => {
     const { userId, isAuthenticated } = useContext(AuthContext);
     const {mattressId } = useParams();
+    const {deleteMattress} = useMattressContex();
+    const [show, setShow] =useState(false);
     const [ mattress, setMattress ] = useState({});
     const mattressService = useService(mattressServiceFactory);
     const navigate = useNavigate();
@@ -21,14 +27,38 @@ export const Details = () => {
 
     const isOwner = mattress._ownerId === userId; 
 
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    
     const onDeleteClick = async() => {
-        
-        await mattressService.delete(mattress._id);
-
-        navigate('/catalog');
+            
+              await mattressService.delete(mattress._id);
+      
+              deleteMattress(mattress._id);
+      
+              navigate('/catalog');
+          
     }
 
     return(
+        <>
+        {show && (
+
+         <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}>
+      <Modal.Dialog>
+        <Modal.Header >
+          <Modal.Title>Are you sure you want to delete mattress {mattress.brand}?</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Footer>
+          <Button onClick={handleClose} variant="secondary">Close</Button>
+          <Button onClick={onDeleteClick} variant="primary">Delete</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+        )}
         <section id="detailsPage">
     <div className="wrapper">
         <div className="mattressCover">
@@ -37,17 +67,18 @@ export const Details = () => {
         <div className="mattressInfo">
             <div className="mattressText">
                 <h1>Brand: {mattress.brand}</h1>
-                <h3><strong>Category:</strong> {mattress.cattegory}</h3>
+                <h3><strong>Category:</strong> {mattress.category}</h3>
                 <h3><strong>Size:</strong> {mattress.size}</h3>
-                <h3><strong>Price:</strong> {mattress.price}</h3>
+                <h3><strong>Price:</strong> {mattress.price}$</h3>
              
                 <p><strong>Description:</strong>{mattress.summary}</p>
             </div>
+      
                 {isOwner && (
 
                 <div className="actionBtn">
                     <Link to={`/catalog/${mattress._id}/edit`} className="edit">Edit</Link>
-                    <button onClick={onDeleteClick}>Delete</button>
+                    <button onClick={handleShow}>Delete</button>
                 
                 </div>
                  )}
@@ -62,5 +93,6 @@ export const Details = () => {
         </div>
     </div>
 </section>
+</>
     )
 }
