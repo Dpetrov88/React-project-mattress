35849e1ab6555dfd0b931a0a19d3f588c5d3dd6f
Dpetrov88 <117352418+Dpetrov88@users.noreply.github.com>
2023-04-04 16:10:38 +0300

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -19,11 +19,35 @@ export const Register = () => {
                 <fieldset>
                     <legend>Register</legend>
                     <label htmlFor="email" className="vhod">Email</label>
-                    <input value={values.email} onChange={changeHandler} id="email" className="email" name="email" type="text" placeholder="Email" />
+                    <input
+                     id="email"
+                     className="email"
+                     name="email"
+                     type="email"
+                     value={values.email}
+                     onChange={changeHandler}
+                     placeholder="Email" />
+
                     <label htmlFor="password" className="vhod">Password</label>
-                    <input value={values.password} onChange={changeHandler} id="password" className="password" name="password" type="password" placeholder="Password" />
+                    <input
+                      id="password" 
+                      className="password" 
+                      name="password" 
+                      type="password" 
+                      value={values.password}
+                      onChange={changeHandler}
+                      placeholder="Password" />
+
                     <label htmlFor="conf-pass" className="vhod">Confirm Password:</label>
-                    <input value={values.repeatPass} onChange={changeHandler} id="conf-pass" className="conf-pass" name="confirmPass" type="password" placeholder="Confirm Password" />
+                    <input 
+                    id="conf-pass" 
+                    className="conf-pass" 
+                    name="confirmPass" 
+                    type="password" 
+                    value={values.repeatPass} 
+                    onChange={changeHandler} 
+                    placeholder="Confirm Password" />
+
                     <button type="submit" className="register">Register</button>
                     <p className="field">
                         <span>If you already have profile click <Link to={'/login'}>here</Link></span>
