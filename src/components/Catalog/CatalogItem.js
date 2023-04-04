37c78fa805d5f37cdb51eba6509d39eff6d112ae
Dpetrov88 <117352418+Dpetrov88@users.noreply.github.com>
2023-04-04 13:01:32 +0300

    Update CatalogItem.js

--- src/components/Catalog/CatalogItem.js
+++ src/components/Catalog/CatalogItem.js
@@ -14,7 +14,7 @@ export const CatalogItem = ({
             <img src={image} alt="./images/back.webp" />
             <p><strong>Brand: </strong><span className="title"><strong>{brand}</strong></span> </p>
             <p><strong>Category: </strong><span className="title"><strong>{category}</strong></span> </p>
-            <p><strong>Price:</strong><span className="salary"><strong>${price}</strong></span></p>
+            <p><strong>Price:</strong><span className="salary"><strong>{price}$</strong></span></p>
             <Link to={`/catalog/${_id}`} className="details-button" >Details</Link>
         </div>
        
